53c045312f675240ee2940f315446a180c377151
JJ <jianan.beijing@gmail.com>
2017-09-19 00:43:23 -0700

    SPA route works

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,34 +1,36 @@
-import React from "react";
+// import React from "react";
 
-import { Router, Route, Switch } from 'react-router'；
+// import { Router, Route, Switch } from "react-router";
 
-// // Include the Route component for displaying individual routes
-// var Route = router.Route;
+// // // Include the Route component for displaying individual routes
+// // var Route = router.Route;
 
-// // Include the Router component to contain all our Routes
-// // Here where we can pass in some configuration as props
-// var Router = router.Router;
+// // // Include the Router component to contain all our Routes
+// // // Here where we can pass in some configuration as props
+// // var Router = router.Router;
 
-// Include the hashHistory prop to handle routing client side without a server
-// https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#hashhistory
-var hashHistory = router.hashHistory;
+// // Include the hashHistory prop to handle routing client side without a server
+// // https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#hashhistory
+// var hashHistory = Router.hashHistory;
 
-// browserHistory makes for cleaner URLs but can only be used when running a server
-//var browserHistory = router.browserHistory;
+// // browserHistory makes for cleaner URLs but can only be used when running a server
+// //var browserHistory = router.browserHistory;
 
-// Include the IndexRoute (catch-all route)
-var IndexRoute = router.IndexRoute;
+// // Include the IndexRoute (catch-all route)
+// var IndexRoute = Router.IndexRoute;
 
-// Reference the high-level components
+import React from 'react';
+import {ReactDOM, render} from 'react-dom';
 import Main from "../components/Main";
 import Search from "../components/Search";
 import Saved from "../components/Saved";
+import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router';
 
-// Export the Routes
-export default (
+
+module.exports=(
 
   // The high level component is the Router component
-  <Router history={hashHistory}>
+  <Router>
     <Route path="/" component={Main}>
 
       {/* If user selects Info or Chat show the appropriate component */}
