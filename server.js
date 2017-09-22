300fa1c08d3f20b24f01541ef9ed1e9102504975
JJ <jianan.beijing@gmail.com>
2017-09-22 00:25:42 -0700

    update cloud database url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static("public"));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("mongodb://localhost/nytreact");
+mongoose.connect("https://tranquil-forest-45064.herokuapp.com/");
 var db = mongoose.connection;
 
 db.on("error", function(err) {
